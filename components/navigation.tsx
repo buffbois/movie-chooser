c043d0abe4aae33eb0aae6e88bd1bd0efb417f73
Sidhant Puntambekar <sidhantnp@yahoo.com>
2021-12-29 17:49:43 -0700

    Add bg-pink-200 and text-pink-200

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/Navigation.module.css'
 export default function Navigation() {
     return (
         <div className='min-h-screen flex bg-purple-300 m-0 sticky top-0 pl-5 pt-3'>
-            <h1 className='inline-flex font-cubano text-pink pr-14 m-0 align-middle text-5xl w-full md:w-auto'>MOVIE CHOOSER</h1>
+            <h1 className='inline-flex font-cubano text-pink-200 pr-14 m-0 align-middle text-5xl w-full md:w-auto'>MOVIE CHOOSER</h1>
             <div className='bg-purple-300 pl-14 ml-14 top-0 w-screen md:w-full'>
-                <nav className='bg-pink align-middle w-screen md:w-auto'>
+                <nav className='bg-pink-200 align-middle w-screen md:w-auto'>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Home</a>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Group Stats</a>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Recommendations</a>
