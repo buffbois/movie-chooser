ae8ad0ceefccaf9a22e8d2be358016dcd70f38d4
Peter Lande <Peter.Lande@colorado.edu>
2021-12-30 12:41:21 -0600

    Added partially functionality to navbar.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,4 +1,4 @@
-import styles from '../styles/Navigation.module.css'
+import Link from 'next/link'
 
 export default function Navigation() {
     return (
@@ -7,14 +7,18 @@ export default function Navigation() {
                 <h1 className='inline-block font-cubano text-pink-200 mr-10 align-middle text-5xl min-w-max'>MOVIE CHOOSER</h1>
             </div>
             <div className='col-span-3 bg-pink-200 flex justify-start'>
-                <div className=''>
-                    <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Home</a>
+                <div>
+                    <Link href="/">
+                        <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Home</a>
+                    </Link>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Group Stats</a>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Recommendations</a>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>My Stats</a>
                 </div>
                 <div className='w-perfect flex justify-end'>
-                    <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Login/Sign-Up</a>
+                    <Link href="/account">
+                        <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Login/Sign-Up</a>
+                    </Link>
                 </div>
             </div>
         </nav>
