22cbc0f5269d0a74e8021e7d289d4d97171367c4
Peter Lande <Peter.Lande@colorado.edu>
2021-12-30 12:53:55 -0600

    Added lighter color on current page in nav bar.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,14 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Navigation() {
+
+    const router = useRouter();
+    const toggledClass = "bg-pink-100 inline-block font-aktiv py-3.5 px-7 hover:bg-teal";
+    const untoggledClass = "inline-block font-aktiv py-3.5 px-7 hover:bg-teal";
+    const homeClass = router.pathname == "/" ? toggledClass : untoggledClass;
+    const signinClass = router.pathname == "/account" ? toggledClass : untoggledClass;
+
     return (
         <nav className='grid grid-rows-1 grid-cols-4 bg-purple-300 py-5 pl-2 pr-0 sticky top-0 z-50'>
             <div className='col-span-1'>
@@ -9,7 +17,7 @@ export default function Navigation() {
             <div className='col-span-3 bg-pink-200 flex justify-start'>
                 <div>
                     <Link href="/">
-                        <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Home</a>
+                        <a className={homeClass}>Home</a>
                     </Link>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Group Stats</a>
                     <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Recommendations</a>
@@ -17,7 +25,7 @@ export default function Navigation() {
                 </div>
                 <div className='w-perfect flex justify-end'>
                     <Link href="/account">
-                        <a className='inline-block font-aktiv py-3.5 px-7 hover:bg-teal'>Login/Sign-Up</a>
+                        <a className={signinClass}>Login/Sign-Up</a>
                     </Link>
                 </div>
             </div>
